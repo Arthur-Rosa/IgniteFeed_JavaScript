44c0f28f2ddb4309d0950b28ef7267b6ce3e1cdb
Matheus <matheuus412@gmail.com>
2022-07-17 20:14:35 -0300

    Atualização para o deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Post } from "./components/Post"
 
 import './global.css';
 import styles from './App.module.css';
-import { Sidebar } from "./components/sidebar";
+import { Sidebar } from "./components/Sidebar";
 
 const posts = [
   {
