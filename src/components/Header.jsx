3d11cb6dd36db779ce07003e9656fdbaf8256250
Matheus <matheuus412@gmail.com>
2022-07-17 20:18:25 -0300

    Atualização para o deploy 1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import styles from './Header.module.css'
 
-import igniteLogo from '../assets/ignite-logo.svg'
+import igniteLogo from '../assets/Ignite-logo.svg'
 
 export function Header() {
     return (
